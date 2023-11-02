0f8f44d58abe978fd4c17b53b502341718320be9
Logan Losee <logan.losee99@gmail.com>
2023-11-01 22:35:39 -0600

    svg renders to new file

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const { Shape, Triangle, Circle, Square } = require('./lib/shapes');
-//import { Shape, Triangle, Circle, Square } from './lib/shapes.js'
+const fs = require('fs');
 
-//fs write file
-
-
-// Function to prompt the user for shape and color
+// Function to prompt the user for shape, shape color, text, and text color
 async function getUserInput() {
-    const answers = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Select a shape:',
-        choices: ['Triangle', 'Circle', 'Square'],
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter a color for the shape:',
+    },
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to 3 letters:',
+      validate: (input) => {
+        if (input.length <= 3) {
+          return true;
+        }
+        return 'Please enter up to 3 letters.';
       },
-      {
-        type: 'input',
-        name: 'color',
-        message: 'Enter a color:',
-      },
-    ]);
-    return answers;
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter a color for the text:',
+    },
+  ]);
+  return answers;
 }
 
-getUserInput();
+// Function to create and render the selected shape with text and text color
+function createAndRenderShape(shape, color, text, textColor) {
+  let selectedShape;
+
+  switch (shape) {
+    case 'Triangle':
+      selectedShape = new Triangle();
+      break;
+    case 'Circle':
+      selectedShape = new Circle();
+      break;
+    case 'Square':
+      selectedShape = new Square();
+      break;
+    default:
+      console.log('Invalid shape selected.');
+      return;
+  }
+
+  selectedShape.setColor(color);
+  selectedShape.setText(text);
+  selectedShape.setTextColor(textColor);
+
+  return selectedShape.render();
+}
+
+// Main function to run the program
+async function main() {
+  console.log('Welcome to the Shape Renderer');
+  const userInput = await getUserInput();
+  const svgString = createAndRenderShape(
+    userInput.shape,
+    userInput.color,
+    userInput.text,
+    userInput.textColor
+  );
+
+  if (svgString) {
+    // Save the SVG to a file
+    fs.writeFileSync('output.svg', svgString);
+    console.log('SVG file saved as "output.svg".');
+  }
+}
+
+main();
