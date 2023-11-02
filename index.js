33d9f174bf5ac0938744a11420197434c3bbf98e
Logan Losee <logan.losee99@gmail.com>
2023-11-01 22:09:14 -0600

    dependencies fix

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const inquirer = require('inquirer');
-const { Shape, Triangle, Circle, Square } = require('./shapes');
+const inquirer = require('inquirer')
+const { Shape, Triangle, Circle, Square } = require('./lib/shapes');
+//import { Shape, Triangle, Circle, Square } from './lib/shapes.js'
+
+//fs write file
+
+
+// Function to prompt the user for shape and color
+async function getUserInput() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape:',
+        choices: ['Triangle', 'Circle', 'Square'],
+      },
+      {
+        type: 'input',
+        name: 'color',
+        message: 'Enter a color:',
+      },
+    ]);
+    return answers;
+}
+
+getUserInput();
